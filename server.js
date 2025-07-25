b2fc361226ecc07c0fbead4c95a9f6b10ed1b40d
Fakhri Fitra <fitrafakhri49@gmail.com>
2025-07-25 15:59:33 +0700

    hapus benerin ry

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(
 app.use(express.json({ limit: "10mb" }));
 
 ry.config({
-  cloud_name: process.env._NACLOUDME,
+  cloud_name: process.env.CLOUD_NAME,
   api_key: procescloudinas.env.API_KEY,
   api_secret: process.env.API_SECRET,
 });
